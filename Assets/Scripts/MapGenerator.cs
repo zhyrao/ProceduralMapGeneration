46f912a8c875e56f916ea53b2aa5fcd68b967da8
zhyrao <rain330047@outlook.com>
2019-02-12 22:53:16 +0800

    Using Perlin Noise Generate Noise Map

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+    public void OnValidate()
+    {
+        if (mapWidth < 1)
+            mapWidth = 1;
+
+        if (mapHight < 1)
+            mapHight = 1;
+    }
+}
